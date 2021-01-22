bfa30d36a3d2e85936569539930e5c62e3167e99
Jet Fontanilla <jetlogs@gmail.com>
2021-01-22 21:14:35 +0800

    unit tests for GCP translate endpoint

--- src/language/language.controller.spec.ts
+++ src/language/language.controller.spec.ts
@@ -1,18 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { GcpTranslateService } from './gcp-translate.service';
 import { LanguageController } from './language.controller';
 
 describe('LanguageController', () => {
-  let controller: LanguageController;
+  let languageController: LanguageController;
+  let gcpTranslateService: GcpTranslateService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [LanguageController],
+      providers: [GcpTranslateService]
     }).compile();
 
-    controller = module.get<LanguageController>(LanguageController);
+    languageController = module.get<LanguageController>(LanguageController);
+    gcpTranslateService = module.get<GcpTranslateService>(GcpTranslateService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(languageController).toBeDefined();
   });
+
+  describe('translate', () => {
+
+    it('should translate using text and target language', async () => {
+      const translatedText = "hola mundo";
+      jest.spyOn(gcpTranslateService, 'translate').mockImplementation(() => Promise.resolve(translatedText));
+
+      const translateRequest = {
+        text: 'hello word',
+        to: 'es'
+      };
+      expect(await languageController.translate(translateRequest)).toBe(translatedText);
+    });
+
+    it('should translate using text and target language and source language', async () => {
+      const translatedText = "hola mundo";
+      jest.spyOn(gcpTranslateService, 'translate').mockImplementation(() => Promise.resolve(translatedText));
+
+      const translateRequest = {
+        text: 'ハローワールド',
+        to: 'es',
+        from: 'ja'
+      };
+      expect(await languageController.translate(translateRequest)).toBe(translatedText);
+    });
+
+  });
+
 });
